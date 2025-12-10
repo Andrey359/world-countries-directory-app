845bc9c210eda5c3686f5ed5f29c05c5a2585178
Andrey359 <74356435+Andrey359@users.noreply.github.com>
2025-12-10 11:38:38 +0300

    Replace Russian exception file OshivaValidacii with English alias

---
+++ app/src/Model/Exceptions/ValidationExceptionRu.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Exceptions;
+
+// Deprecated: Use ValidationException instead
+// This file kept only for backward compatibility
+
+class OshivaValidacii extends ValidationException
+{
+}

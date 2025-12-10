a03d4f06ae9efc97b662d3324b948f461835a3bd
Andrey359 <74356435+Andrey359@users.noreply.github.com>
2025-12-10 11:39:10 +0300

    Replace Russian exception file with English alias for DuplicateCountryException

---
+++ app/src/Model/Exceptions/DuplicateCountryExceptionRu.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Exceptions;
+
+// Deprecated: Use DuplicateCountryException instead
+// This file kept only for backward compatibility
+
+class DuplicateCountryExceptionRu extends DuplicateCountryException
+{
+}

6e70f1cfabb47497e9a1cb73659ada9ffe7069e5
Andrey359 <74356435+Andrey359@users.noreply.github.com>
2025-12-10 11:38:52 +0300

    Replace Russian exception file with English alias for CountryNotFoundException

---
+++ app/src/Model/Exceptions/CountryNotFoundExceptionRu.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Exceptions;
+
+// Deprecated: Use CountryNotFoundException instead
+// This file kept only for backward compatibility
+
+class CountryNotFoundExceptionRu extends CountryNotFoundException
+{
+}
